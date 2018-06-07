f774b8e8345c22abb5972562968d469531cbd0d6
CoffeeCatRailway <dragonlee1002@gmail.com>
2018-06-07 16:04:09 +1000

    Moved world types to CheeseMod::postInit

--- src/main/java/coffeecatteam/cheesemod/CheeseMod.java
+++ src/main/java/coffeecatteam/cheesemod/CheeseMod.java
@@ -1,19 +1,19 @@
 package coffeecatteam.cheesemod;
 
-import org.apache.logging.log4j.Logger;
-
 import coffeecatteam.cheesemod.config.Config;
 import coffeecatteam.cheesemod.objects.tabs.TabCheese;
 import coffeecatteam.cheesemod.objects.tabs.TabCheeseArmorTools;
 import coffeecatteam.cheesemod.objects.tabs.TabCheeseFoods;
 import coffeecatteam.cheesemod.proxy.ProxyCommon;
-import coffeecatteam.cheesemod.util.OnPlayerJoin;
+import coffeecatteam.cheesemod.util.Events;
 import coffeecatteam.cheesemod.util.OreDictionaries;
 import coffeecatteam.cheesemod.util.Utils;
 import coffeecatteam.cheesemod.util.handlers.GuiHandler;
-import coffeecatteam.cheesemod.util.handlers.RegistryHandler;
+import coffeecatteam.cheesemod.world.types.WorldTypeCheese;
+import coffeecatteam.cheesemod.world.types.WorldTypeHam;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.launchwrapper.Launch;
+import net.minecraft.world.WorldType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -22,11 +22,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
+import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY)
 public class CheeseMod {
 
-	public static final CreativeTabs CHEESETAB = new TabCheese("cheesetab");
+    public static final CreativeTabs CHEESETAB = new TabCheese("cheesetab");
 	public static final CreativeTabs CHEESEARMORTOOLSTAB = new TabCheeseArmorTools("cheesearmortoolstab");
 	public static final CreativeTabs CHEESEFOODSTAB = new TabCheeseFoods("cheesefoodstab");
 
@@ -63,8 +64,9 @@ public class CheeseMod {
 	@EventHandler
 	public static void postInit(FMLPostInitializationEvent event) {
 		proxy.postInit(event);
-		RegistryHandler.postInitRegistries();
-        MinecraftForge.EVENT_BUS.register(new OnPlayerJoin());
+        WorldType CHEESE = new WorldTypeCheese();
+        WorldType HAM = new WorldTypeHam();
+        MinecraftForge.EVENT_BUS.register(new Events());
 
 		logger.info("Post Initialize");
 	}
