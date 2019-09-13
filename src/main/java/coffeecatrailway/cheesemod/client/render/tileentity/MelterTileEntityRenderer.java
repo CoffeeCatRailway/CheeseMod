b7f0d41ade43724dc1ce982b46c48a631d0c779d
CoffeeCatRailway <dragonlee1002@gmail.com>
2019-09-13 10:13:38 +1000

    Water in melter now renders with biome colors

--- src/main/java/coffeecatrailway/cheesemod/client/render/tileentity/MelterTileEntityRenderer.java
+++ src/main/java/coffeecatrailway/cheesemod/client/render/tileentity/MelterTileEntityRenderer.java
@@ -6,7 +6,6 @@ import coffeecatrailway.cheesemod.tileentity.MelterTileEntity;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.fluid.Fluid;
-import net.minecraft.fluid.Fluids;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
@@ -40,17 +39,16 @@ public class MelterTileEntityRenderer extends TileEntityRenderer<MelterTileEntit
             float yOff = MelterScreen.mapFluid(fluidAmount, 0.0f, 0.1f);
             GlStateManager.translatef((float) x, (float) y + 0.26f + yOff, (float) z); /// TODO: Fix texture stretching! ///
 
-            if (fluid.isEquivalentTo(Fluids.WATER))
-                GlStateManager.color4f(0.23922F, 0.42745F, 1.0F, 1.0F);
+            int color = fluid.getAttributes().getColor(this.getWorld(), tile.getPos());
+            float[] colors = new float[]{((color >> 16) & 0xFF) / 255.0f, ((color >> 8) & 0xFF) / 255.0f, (color & 0xFF) / 255.0f, ((color >> 24) & 0xFF) / 255.0f};
+            GlStateManager.color4f(colors[0], colors[1], colors[2], colors[3]);
 
             GlStateManager.enableBlend();
             this.bindTexture(fluidTexture);
-
             fluidModel.render();
             GlStateManager.disableBlend();
 
-            if (fluid.isEquivalentTo(Fluids.WATER))
-                GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
             GlStateManager.popMatrix();
         }
     }
