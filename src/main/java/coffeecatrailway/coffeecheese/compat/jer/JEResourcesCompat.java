1729130d6cf716724eaef4b0b428e56ac9ac566f
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-01-07 17:53:53 +1100

    Pineapple jer

--- src/main/java/coffeecatrailway/coffeecheese/compat/jer/JEResourcesCompat.java
+++ src/main/java/coffeecatrailway/coffeecheese/compat/jer/JEResourcesCompat.java
@@ -5,10 +5,15 @@ import coffeecatrailway.coffeecheese.common.entity.CheeseFoodie;
 import coffeecatrailway.coffeecheese.common.entity.GrilledCheeseFoodie;
 import coffeecatrailway.coffeecheese.common.entity.HamCookedFoodie;
 import coffeecatrailway.coffeecheese.common.entity.HamRawFoodie;
+import coffeecatrailway.coffeecheese.registry.ModBlocks;
+import coffeecatrailway.coffeecheese.registry.ModItems;
 import jeresources.api.IJERAPI;
 import jeresources.api.JERPlugin;
 import jeresources.api.conditionals.LightLevel;
+import jeresources.api.drop.PlantDrop;
 import jeresources.compatibility.JERAPI;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
 
 /**
  * @author CoffeeCatRailway
@@ -24,6 +29,7 @@ public class JEResourcesCompat {
     public static void register() {
         if (jerAPI == null) jerAPI = JERAPI.getInstance();
 
+        /// Mobs ///
         CheeseFoodie cheeseFoodie = new CheeseFoodie(null);
         jerAPI.getMobRegistry().register(cheeseFoodie, LightLevel.any, cheeseFoodie.getLootTableResourceLocation());
 
@@ -36,7 +42,11 @@ public class JEResourcesCompat {
         HamCookedFoodie hamCookedFoodie = new HamCookedFoodie(null);
         jerAPI.getMobRegistry().register(hamCookedFoodie, LightLevel.any, hamCookedFoodie.getLootTableResourceLocation());
 
+        /// Structures ///
         jerAPI.getDungeonRegistry().registerCategory(CAT_PINE_HUT, "Pine Hut");
         jerAPI.getDungeonRegistry().registerChest(CAT_PINE_HUT, CheeseMod.PINE_HUT_LOOT_TABLE);
+
+        /// Plants ///
+        jerAPI.getPlantRegistry().registerWithSoil(ModBlocks.PINEAPPLE.get(), Blocks.FARMLAND.getDefaultState(), new PlantDrop(new ItemStack(ModItems.PINEAPPLE.get()), 0.5f));
     }
 }
