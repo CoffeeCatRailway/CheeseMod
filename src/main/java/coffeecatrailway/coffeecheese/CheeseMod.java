525bf7be746d158cbb1aedefd985f8367e37bd2f
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-01-28 14:23:23 +1100

    Fixed item dupe bug

--- src/main/java/coffeecatrailway/coffeecheese/CheeseMod.java
+++ src/main/java/coffeecatrailway/coffeecheese/CheeseMod.java
@@ -20,19 +20,25 @@ import coffeecatrailway.coffeecheese.compat.top.TOPCompatibility;
 import coffeecatrailway.coffeecheese.registry.*;
 import com.mrcrayfish.filters.Filters;
 import io.netty.buffer.Unpooled;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.BreakingParticle;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.DimensionManager;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.event.world.RegisterDimensionsEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -66,8 +72,6 @@ public class CheeseMod {
     public static ModCheeseConfig.ClientConfig CLIENT_CFG;
     public static ModCheeseConfig.ServerConfig SERVER_CFG;
 
-    public static final ResourceLocation PINE_HUT_LOOT_TABLE = CheeseMod.getLocation("chests/pine_hut");
-
     public CheeseMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::setupClient);
@@ -99,7 +103,7 @@ public class CheeseMod {
     }
 
     public void interModEvent(InterModProcessEvent event) {
-        if (ModList.get().isLoaded("jeresources"))
+        if (ModList.get().isLoaded(jeresources.JEResources.ID))
             JEResourcesCompat.register();
     }
 
@@ -116,7 +120,7 @@ public class CheeseMod {
 
     @OnlyIn(Dist.CLIENT)
     public static void registerFilters() {
-        if (ModList.get().isLoaded("filters")) {
+        if (ModList.get().isLoaded(com.mrcrayfish.filters.Reference.MOD_ID)) {
             Filters.get().register(ModItemGroups.GROUP_ALL, CheeseMod.getLocation("filters/all/natural"), new ItemStack(ModBlocks.CHEESE_SAPLING.get()));
             Filters.get().register(ModItemGroups.GROUP_ALL, CheeseMod.getLocation("filters/all/woods"), new ItemStack(ModBlocks.CHEESE_LOG.get()));
             Filters.get().register(ModItemGroups.GROUP_ALL, CheeseMod.getLocation("filters/all/minerals"), new ItemStack(ModBlocks.CHEESE_METAL_BLOCK.get()));
@@ -138,6 +142,8 @@ public class CheeseMod {
     @OnlyIn(Dist.CLIENT)
     public static void registerParticleFactories() {
         Minecraft.getInstance().particles.registerFactory(ModParticles.ITEM_CHEESE_BALL.get(), (type, world, v, v1, v2, v3, v4, v5) -> new BreakingParticle(world, v, v1, v2, new ItemStack(ModItems.CHEESE_BALL.get())));
+
+        CheeseMod.LOGGER.debug("Particles");
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -151,6 +157,8 @@ public class CheeseMod {
         RenderingRegistry.registerEntityRenderingHandler(HamCookedFoodie.class, manager -> new FoodieRenderer<>(manager, CheeseMod.getLocation("textures/entity/foodie/ham_cooked.png")));
 
         RenderingRegistry.registerEntityRenderingHandler(BoatEntityCM.class, BoatRendererCM::new);
+
+        CheeseMod.LOGGER.debug("Entity renderers");
     }
 
     @OnlyIn(Dist.CLIENT)
@@ -159,6 +167,8 @@ public class CheeseMod {
         ClientRegistry.bindTileEntitySpecialRenderer(MelterTileEntity.class, new MelterTileEntityRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(PizzaOvenTileEntity.class, new PizzaOvenTileEntityRenderer());
         ClientRegistry.bindTileEntitySpecialRenderer(FoodWorldPortalTileEntity.class, new FoodWorldPortalTileEntityRenderer());
+
+        CheeseMod.LOGGER.debug("Tile entity renderers");
     }
 
     public void setupCommon(FMLCommonSetupEvent event) {
@@ -167,8 +177,8 @@ public class CheeseMod {
         ModWorldFeatures.addFeatures();
         ModVanillaCompat.setup();
 
-        if (ModList.get().isLoaded("theoneprobe"))
-            InterModComms.sendTo("theoneprobe", "getTheOneProbe", TOPCompatibility::new);
+        if (ModList.get().isLoaded(mcjty.theoneprobe.TheOneProbe.MODID))
+            InterModComms.sendTo(mcjty.theoneprobe.TheOneProbe.MODID, "getTheOneProbe", TOPCompatibility::new);
 
         CheeseMod.LOGGER.debug("Common setup");
     }
@@ -177,8 +187,48 @@ public class CheeseMod {
     public static class ForgeEventBus {
 
         @SubscribeEvent
+        public static void onItemCrafted(PlayerEvent.ItemCraftedEvent event) {
+            if (event.getCrafting().getItem() == ModFluids.OIL_BUCKET.get())
+                event.getInventory().decrStackSize(getSlotFor(event, new ItemStack(Items.WATER_BUCKET)), 1);
+        }
+
+        /**
+         * Copied from {@link net.minecraft.entity.player.PlayerInventory}
+         */
+        private static int getSlotFor(PlayerEvent.ItemCraftedEvent event, ItemStack stack) {
+            for (int i = 0; i < event.getInventory().getSizeInventory(); ++i)
+                if (!event.getInventory().getStackInSlot(i).isEmpty() && stackEqualExact(stack, event.getInventory().getStackInSlot(i)))
+                    return i;
+            return -1;
+        }
+
+        /**
+         * Copied from {@link net.minecraft.entity.player.PlayerInventory}
+         */
+        private static boolean stackEqualExact(ItemStack stack1, ItemStack stack2) {
+            return stack1.getItem() == stack2.getItem() && ItemStack.areItemStackTagsEqual(stack1, stack2);
+        }
+
+        @SubscribeEvent
+        public static void onBlockBroken(BlockEvent.BreakEvent event) {
+            if (!event.getWorld().isRemote()) {
+                if ((event.getPlayer().getHeldItemMainhand().getItem() != Items.SHEARS) || (!event.getPlayer().isCreative())) {
+                    if (event.getState().getBlock() == Blocks.GRASS || event.getState().getBlock() == Blocks.TALL_GRASS || event.getState().getBlock() == Blocks.FERN) {
+                        if (Math.random() <= (double) ModCheeseConfig.pineappleDropChance.get() / 100) {
+                            event.getWorld().setBlockState(event.getPos(), Blocks.AIR.getDefaultState(), 2);
+                            event.getWorld().addEntity(new ItemEntity((World) event.getWorld(), event.getPos().getX(),
+                                    event.getPos().getY(), event.getPos().getZ(), new ItemStack(ModItems.PINEAPPLE_PLANT.get(), 1)));
+                        }
+                    }
+                }
+            }
+        }
+
+        @SubscribeEvent
         public static void serverStarting(FMLServerStartingEvent event) {
             ChezCommand.register(event.getCommandDispatcher());
+
+            CheeseMod.LOGGER.debug("Command registered");
         }
 
         @SubscribeEvent
@@ -215,7 +265,7 @@ public class CheeseMod {
     }
 
     public static ResourceLocation getLocation(String path) {
-        return new ResourceLocation(MOD_ID, path);
+        return new ResourceLocation(CheeseMod.MOD_ID, path);
     }
 
     public static float map(float from, float fromMin, float fromMax, float toMin, float toMax) {
