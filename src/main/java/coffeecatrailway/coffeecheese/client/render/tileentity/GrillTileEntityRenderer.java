1a174ce8aa8a42ceb4dcc747acb64ea3e167a255
CoffeeCatRailway <dragonlee1002@gmail.com>
2020-02-05 14:27:52 +1100

    Fixed fluid renders on melter & grill

--- src/main/java/coffeecatrailway/coffeecheese/client/render/tileentity/GrillTileEntityRenderer.java
+++ src/main/java/coffeecatrailway/coffeecheese/client/render/tileentity/GrillTileEntityRenderer.java
@@ -1,19 +1,33 @@
 package coffeecatrailway.coffeecheese.client.render.tileentity;
 
+import coffeecatrailway.coffeecheese.CheeseMod;
 import coffeecatrailway.coffeecheese.common.block.GrillBlock;
 import coffeecatrailway.coffeecheese.common.tileentity.GrillTileEntity;
 import coffeecatrailway.coffeecheese.registry.ModItems;
 import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fluids.FluidAttributes;
+import net.minecraftforge.fluids.FluidStack;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
 
 /**
  * @author CoffeeCatRailway
  * Created: 8/01/2020
+ *
+ * Parts copied from https://github.com/InnovativeOnlineIndustries/Industrial-Foregoing/blob/1.14/src/main/java/com/buuz135/industrial/proxy/client/render/FluidConveyorTESR.java
  */
 @OnlyIn(Dist.CLIENT)
 public class GrillTileEntityRenderer extends TileEntityRenderer<GrillTileEntity> {
@@ -25,6 +39,40 @@ public class GrillTileEntityRenderer extends TileEntityRenderer<GrillTileEntity>
             GlStateManager.translated(x + 0.5d, y + 0.875d, z + 0.5d);
             Minecraft.getInstance().getItemRenderer().renderItem(new ItemStack(ModItems.OIL_CATCHER.get()), ItemCameraTransforms.TransformType.NONE);
             GlStateManager.popMatrix();
+
+            if (tile.data.get(5) > 0) {
+                GlStateManager.pushMatrix();
+                GlStateManager.translatef((float) x, (float) y, (float) z);
+
+                Tessellator tessellator = Tessellator.getInstance();
+                BufferBuilder buffer = tessellator.getBuffer();
+                this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+                RenderHelper.disableStandardItemLighting();
+                GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+                GlStateManager.enableBlend();
+                GlStateManager.disableCull();
+
+                if (Minecraft.isAmbientOcclusionEnabled())
+                    GlStateManager.shadeModel(GL11.GL_SMOOTH);
+                else
+                    GlStateManager.shadeModel(GL11.GL_FLAT);
+
+                buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
+                FluidStack fluid = tile.getTank().getFluid();
+                TextureAtlasSprite still = Minecraft.getInstance().getTextureMap().getAtlasSprite(fluid.getFluid().getAttributes().getStill(fluid).toString());
+                double posY = 7.1f / 16f + CheeseMod.map(tile.data.get(5), 0f, FluidAttributes.BUCKET_VOLUME, 0f, .85f) / 16f;
+                double right = 2 / 16f;
+                double left = 14 / 16f;
+                Color color = new Color(fluid.getFluid().getAttributes().getColor(tile.getTank().getFluid()));
+
+                buffer.pos(right, posY, right).tex(still.getInterpolatedU(0), still.getInterpolatedV(0)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+                buffer.pos(left, posY, right).tex(still.getInterpolatedU(16), still.getInterpolatedV(0)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+                buffer.pos(left, posY, left).tex(still.getInterpolatedU(16), still.getInterpolatedV(16)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+                buffer.pos(right, posY, left).tex(still.getInterpolatedU(0), still.getInterpolatedV(16)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+
+                tessellator.draw();
+                GlStateManager.popMatrix();
+            }
         }
     }
 }
--- src/main/java/coffeecatrailway/coffeecheese/client/render/tileentity/MelterTileEntityRenderer.java
+++ src/main/java/coffeecatrailway/coffeecheese/client/render/tileentity/MelterTileEntityRenderer.java
@@ -1,45 +1,65 @@
 package coffeecatrailway.coffeecheese.client.render.tileentity;
 
 import coffeecatrailway.coffeecheese.client.gui.screen.MelterScreen;
-import coffeecatrailway.coffeecheese.client.render.tileentity.model.MelterFluidModel;
 import coffeecatrailway.coffeecheese.common.tileentity.MelterTileEntity;
 import com.mojang.blaze3d.platform.GlStateManager;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.AtlasTexture;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
-import net.minecraft.fluid.Fluid;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fluids.FluidStack;
+import org.lwjgl.opengl.GL11;
+
+import java.awt.*;
 
 /**
  * @author CoffeeCatRailway
  * Created: 6/09/2019
+ * <p>
+ * Parts copied from https://github.com/InnovativeOnlineIndustries/Industrial-Foregoing/blob/1.14/src/main/java/com/buuz135/industrial/proxy/client/render/FluidConveyorTESR.java
  */
 @OnlyIn(Dist.CLIENT)
 public class MelterTileEntityRenderer extends TileEntityRenderer<MelterTileEntity> {
 
-    private final MelterFluidModel fluidModel = new MelterFluidModel();
-
     @Override
     public void render(MelterTileEntity tile, double x, double y, double z, float partialTicks, int destroyStage) {
-        int fluidAmount = tile.getTank().getFluid().getAmount();
-        Fluid fluid = tile.getTank().getFluid().getFluid();
-
-        ResourceLocation fluidTexture = MelterScreen.getFluidTexture(fluid);
-        if (fluidTexture != null && !fluidTexture.toString().equals("minecraft:textures/white.png")) {
+        if (tile.data.get(4) > 0) {
             GlStateManager.pushMatrix();
-            float yOff = MelterScreen.mapFluid(fluidAmount, 0.0f, 0.1f);
-            GlStateManager.translatef((float) x, (float) y + 0.26f + yOff, (float) z); /// TODO: Fix texture stretching! ///
+            GlStateManager.translatef((float) x, (float) y, (float) z);
 
-            int color = fluid.getAttributes().getColor(this.getWorld(), tile.getPos());
-            float[] colors = new float[]{((color >> 16) & 0xFf) / 255.0f, ((color >> 8) & 0xFf) / 255.0f, (color & 0xFf) / 255.0f, ((color >> 24) & 0xFf) / 255.0f};
-            GlStateManager.color4f(colors[0], colors[1], colors[2], colors[3]);
-
+            Tessellator tessellator = Tessellator.getInstance();
+            BufferBuilder buffer = tessellator.getBuffer();
+            this.bindTexture(AtlasTexture.LOCATION_BLOCKS_TEXTURE);
+            RenderHelper.disableStandardItemLighting();
+            GlStateManager.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
             GlStateManager.enableBlend();
-            this.bindTexture(fluidTexture);
-            fluidModel.render();
-            GlStateManager.disableBlend();
+            GlStateManager.disableCull();
 
-            GlStateManager.color4f(1.0f, 1.0f, 1.0f, 1.0f);
+            if (Minecraft.isAmbientOcclusionEnabled())
+                GlStateManager.shadeModel(GL11.GL_SMOOTH);
+            else
+                GlStateManager.shadeModel(GL11.GL_FLAT);
+
+            buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX_COLOR);
+            FluidStack fluid = tile.getTank().getFluid();
+            TextureAtlasSprite still = Minecraft.getInstance().getTextureMap().getAtlasSprite(fluid.getFluid().getAttributes().getStill(fluid).toString());
+            double posY = 4.1f / 16f + MelterScreen.mapFluid(tile.data.get(4), 0f, 2.85f) / 16f;
+            double right = 1 / 16f;
+            double left = 15 / 16f;
+            Color color = new Color(fluid.getFluid().getAttributes().getColor(tile.getTank().getFluid()));
+
+            buffer.pos(right, posY, right).tex(still.getInterpolatedU(0), still.getInterpolatedV(0)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+            buffer.pos(left, posY, right).tex(still.getInterpolatedU(16), still.getInterpolatedV(0)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+            buffer.pos(left, posY, left).tex(still.getInterpolatedU(16), still.getInterpolatedV(16)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+            buffer.pos(right, posY, left).tex(still.getInterpolatedU(0), still.getInterpolatedV(16)).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+
+            tessellator.draw();
             GlStateManager.popMatrix();
         }
     }
